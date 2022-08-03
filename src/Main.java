7df22b4d3699b02ea2aeb39938825fe6635d232a
jd-hyun <wjdehdgus3540@naver.com>
2022-08-03 23:29:14 +0900

    test

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] value = new int[n];
+        int min = 0;
+        for (int i = 0; i < n; i++) 
+            value[i] = scanner.nextInt();
+        for (int i = n - 1; i >= 0; i--) {
+            if (k / value[i] != 0) {
+                min += k / value[i];
+                k %= value[i];
+            }
+        }
+        System.out.println(min);
+        scanner.close();
+    }
+}
